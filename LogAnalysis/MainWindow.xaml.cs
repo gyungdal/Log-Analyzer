2c8d63ff911f2d2d33cd3df6ab27cf647a06bea6
GyungDal <9274aa@gmail.com>
2017-06-15 11:00:03 +0900

    search animation, progress bar

--- LogAnalysis/MainWindow.xaml.cs
+++ LogAnalysis/MainWindow.xaml.cs
@@ -1,14 +1,18 @@
-﻿using System;
+﻿using LogAnalysis.ViewModel;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,12 +24,13 @@ namespace LogAnalysis
     /// </summary>
     public partial class MainWindow : Window
     {
+        private System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainWindowViewModel();
             TextBox_LostFocus(SearchText, null);
         }
-
         private void TextBox_LostFocus(object sender, RoutedEventArgs e)
         {
             if (sender is TextBox)
@@ -39,6 +44,9 @@ namespace LogAnalysis
             }
         }
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+        }
         private void TextBox_GotFocus(object sender, RoutedEventArgs e)
         {
             if (sender is TextBox)
@@ -50,6 +58,11 @@ namespace LogAnalysis
                     ((TextBox)sender).Foreground = Brushes.White;
                 }
             }
+        }
+
+        private void SearchText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ((Storyboard)FindResource("WaitStoryboard")).Begin();
         }
     }
 }
---
+++ LogAnalysis/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace LogAnalysis.ViewModel
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private double _processValue;
+        private DispatcherTimer dispatcherTimer;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public double ProcessValue {
+            get { return _processValue; }
+            set {
+                if (value == _processValue)
+                    return;
+
+                _processValue = value;
+                OnPropertyChanged("ProcessValue");
+            }
+        }
+        public MainWindowViewModel()
+        {
+            dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += (s, e) =>
+            {
+                ProcessValue = new Random().NextDouble() * 100;
+            };
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+        }
+        
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
