549f4dca90a25d47f60f4c70b5021f3bbc56dee2
GyungDal <9274aa@gmail.com>
2017-06-16 15:00:53 +0900

    Change UI!

--- LogAnalysis/MainWindow.xaml.cs
+++ LogAnalysis/MainWindow.xaml.cs
@@ -24,45 +24,10 @@ namespace LogAnalysis
     /// </summary>
     public partial class MainWindow : Window
     {
-        private System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
         public MainWindow()
         {
             InitializeComponent();
             DataContext = new MainWindowViewModel();
-            TextBox_LostFocus(SearchText, null);
-        }
-        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
-        {
-            if (sender is TextBox)
-            {
-                //If nothing was entered, reset default text.
-                if (((TextBox)sender).Text.Trim().Equals(""))
-                {
-                    ((TextBox)sender).Foreground = Brushes.Gray;
-                    ((TextBox)sender).Text = "Search";
-                }
-            }
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-        }
-        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
-        {
-            if (sender is TextBox)
-            {
-                //If nothing has been entered yet.
-                if (((TextBox)sender).Foreground == Brushes.Gray)
-                {
-                    ((TextBox)sender).Text = "";
-                    ((TextBox)sender).Foreground = Brushes.White;
-                }
-            }
-        }
-
-        private void SearchText_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            ((Storyboard)FindResource("WaitStoryboard")).Begin();
         }
     }
 }
