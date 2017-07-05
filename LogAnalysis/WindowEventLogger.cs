4755f84fc80b972808f45c9a79e2d8d581b5d69b
GyungDal <9274aa@gmail.com>
2017-07-05 13:10:11 +0900

    sample

--- LogAnalysis/WindowEventLogger.cs
+++ LogAnalysis/WindowEventLogger.cs
@@ -6,13 +6,13 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Xml;
-
+using System.Diagnostics.Eventing.Reader;
 namespace LogAnalysis
 {
     class WindowEventLogger
     {
         private static WindowEventLogger instance;
-
+        private List<EventRecord> Records;
         public static WindowEventLogger GetInstance(string xmlPath)
         {
             if (instance == null)
@@ -38,22 +38,17 @@ namespace LogAnalysis
 
         public WindowEventLogger(string xmlPath)
         {
-            XmlDocument doc = new XmlDocument();
-            doc.Load(@"C:\Users\Rafael\Desktop\order.xml");
-
-            foreach (XmlNode xn in doc.SelectNodes("/orders/order"))
+            Records = new List<EventRecord>();
+            using (var reader = new EventLogReader(xmlPath, PathType.FilePath))
             {
-
-                string orderDate = xn["order_date"].InnerText;
-                string customer = xn["customer_id"].InnerText;
-                int customerID = Int32.Parse(customer);//convert to integer
-
-                foreach (XmlNode item in doc.SelectNodes("/orders/order/product"))
+                EventRecord record;
+                while ((record = reader.ReadEvent()) != null)
                 {
-                    string productID = item["product_id"].InnerText;
-                    string productQT = item["product_qt"].InnerText;
-                    int product_qt = Int32.Parse(productQT);//convert to integer
-
+                    Records.Add(record);
+                    using (record)
+                    {
+                        Console.WriteLine("{0} {1}: {2}", record.TimeCreated, record.LevelDisplayName, record.FormatDescription());
+                    }
                 }
             }
         }
---
+++ Test ConsoleApp/Test ConsoleApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.Eventing.Reader;
+
+namespace Test_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //var t = from l in LogRecordCollection("e:\\evt.evtx")
+            //        where l.ProviderName.StartsWith("SQL") && l.TimeCreated > new DateTime(2011, 03, 01)
+            //        select l;
+            var t = from l in LogRecordCollection(@"E:\Security.evtx")
+                    where l.TimeCreated > new DateTime(2011, 03, 01)
+                    orderby l.RecordId descending
+                    select l ;
+            foreach(var tt in t)
+            {
+                Console.WriteLine("==================================");
+                Console.WriteLine("ID : " + tt.Id);
+                Console.WriteLine("Activity ID : " + tt.ActivityId);
+                Console.WriteLine("Container Log : " + tt.ContainerLog);
+                Console.WriteLine("Provider Name : " + tt.ProviderName);
+                Console.WriteLine("Task Display Name : " + tt.TaskDisplayName);
+                Console.WriteLine("Machine Name : " + tt.MachineName);
+                Console.WriteLine("Log Name: " + tt.LogName);
+                Console.WriteLine("==================================\n\n");
+ //               Console.WriteLine("XML : " + tt.ToXml());
+            }
+            while (true) ;
+        }
+
+        static IEnumerable<EventLogRecord> LogRecordCollection(string filename, string xpathquery = "*")
+        {
+            var eventLogQuery = new EventLogQuery(filename, PathType.FilePath, xpathquery);
+
+            using (var eventLogReader = new EventLogReader(eventLogQuery))
+            {
+                EventLogRecord eventLogRecord;
+
+                while ((eventLogRecord = (EventLogRecord)eventLogReader.ReadEvent()) != null)
+                    yield return eventLogRecord;
+            }
+        }
+    }
+}
